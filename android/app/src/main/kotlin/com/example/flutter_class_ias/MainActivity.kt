50d5fcb09c7ac01d0bdd24990bfc997ff6792981
zeshansahi <zeshansahi4u@gmail.com>
2025-04-12 21:45:29 +0500

    flutter first practial class

---
+++ android/app/src/main/kotlin/com/example/flutter_class_ias/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.flutter_class_ias
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
---
+++ lib/main.dart
@@ -0,0 +1,73 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  print("Main function call");
+  runApp(
+    MyApp()
+  );
+  
+}
+
+class MyApp extends StatelessWidget{
+
+  @override
+  Widget build(BuildContext context) {
+    print("My app widget rebuild");
+    return
+      MaterialApp(
+          home:Scaffold(
+            drawer: Drawer(
+              child: ListView(
+                children: [
+                  DrawerHeader(
+                    child: Container(color: Colors.blue,
+                    child: Center(child: Text("Drawer")),),
+                  ),
+                ListTile(leading: Icon(Icons.add),title: Text("Add new User"),),
+                ListTile(leading: Icon(Icons.notification_add),title: Text("Notification"),),
+                ListTile(leading: Icon(Icons.settings),title: Text("Settings"),),
+
+                ],
+              ),
+            ),
+            appBar: AppBar(
+              backgroundColor: Colors.blue,
+              title: Text("Hor restart with r"),
+            ),
+            body: Container(
+              color: Colors.blueGrey,
+
+              child: Row(
+                // mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                children: [
+                  Container(
+                      width: 100,
+                      height: 100,
+                      child: Image(image: NetworkImage('https://images.pexels.com/photos/9432524/pexels-photo-9432524.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1')))
+               ,
+                Container(
+                    width:100,
+                    height: 100,
+                    child: Image(image: AssetImage("images/pic_one.jpg")))
+,                Container(
+                    width:100,
+                    height: 100,
+                    child: Image(image: AssetImage("images/pic_two.jpg")))
+                ],
+              ),
+            ),
+            floatingActionButton: FloatingActionButton(onPressed: (){},child: Icon(Icons.add),),
+            bottomNavigationBar: BottomAppBar(
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceAround,
+                children: [
+                  Icon(Icons.add),
+                  Icon(Icons.phone),
+                  Icon(Icons.notification_add),
+                ],
+              ),
+            ),
+          )
+      );
+  }
+}
